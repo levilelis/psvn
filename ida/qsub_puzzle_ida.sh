c5cacc80f36db36031ec8e7b1793e30ad40427da
levilelis <levilelis@gmail.com>
2019-02-08 21:03:52 -0200

    files for experimenting in cluster

---
+++ ida/qsub_puzzle_ida.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#directory in which the problem instances are
+dir=../problem-singles/puzzle_4x4/
+pdb=../pdb/sliding_tile4x4_costs_1_200_123456
+
+for PROBLEM in `dir -d $dir*.pro` ; do
+	INST=$(echo ${PROBLEM} | sed 's/.*\///')
+	INSTNAME=${INST%.*}
+	(qsub -v PDB=$pdb,PROBLEM=$PROBLEM,INSTNAME=$INSTNAME single_puzzle_ida.sh) &
+done
+
+
